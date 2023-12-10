2d63ebed112a584e37f01dee76c8a967ef1482cd
zardan <sara.v.danielsson@gmail.com>
2023-12-10 11:04:36 +0100

    Implement part 2

--- day6/aoc6.py
+++ day6/aoc6.py
@@ -1,10 +1,4 @@
-# Parprogrammering mellan älskling och älskling
-# Båda lovar att hålla gott humör och inte börja sucka/skrika åt varandra
-# Båda älskar varandra och uppskattar varandras olikheter
-
-import bisect
-
-file = open("test_input")
+file = open("input")
 [times, distances] = file.read().split("\n")
 times = times[9:].split(" ")
 times = [int(t) for t in times if len(t)>0]
@@ -14,6 +8,8 @@ distances = [int(d) for d in distances if len(d)>0]
 def dist(total_time, powerup_time):
     return (total_time-powerup_time)*powerup_time
 
+# PART ONE
+
 key = 1
 for time, record in zip(times, distances):
     button_durations = list(range(0,time+1))
@@ -21,9 +17,23 @@ for time, record in zip(times, distances):
     wins = sum(1 for d in distances_for_duration if d > record)
     key *= wins
 
-print("Part1:", key)
+print("PART 1.", key)
 
-time = int("".join([str(t) for t in times]))
+# PART TWO
+
+race_time = int("".join([str(t) for t in times]))
 record = int("".join([str(d) for d in distances]))
 
-print(dist(time, time/2), record)
+t_min = 0
+t_max = int((race_time+1)/2)
+while abs(t_min-t_max)>1:
+    t_mid = int((t_min+t_max)/2)
+    d_mid = dist(race_time, t_mid)
+    if d_mid > record: 
+        t_max = t_mid
+    else: 
+        t_min = t_mid
+
+wins = race_time+1-2*t_max
+
+print("PART 2.", wins)
